0b2d08bba07a4abcddb9e8aed849f07d8da0e2a1
James Kemp <jameskemp2796@yahoo.com>
2019-10-26 12:30:57 -0400

    space changes

--- client/src/pages/ResultList.js
+++ client/src/pages/ResultList.js
@@ -5,6 +5,7 @@ class ResultList extends React.Component {
 		return(
 			<div className="result-list">
 					<h1>This is the ResultList Page</h1>
+					<h2>Added h2 so that it sees the indentation changes</h2>
 			</div>
 		)
 	}
