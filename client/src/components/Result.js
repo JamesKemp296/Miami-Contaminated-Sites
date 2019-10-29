cf46722751f2305595a3722034ad7e755dafb205
James Kemp <jameskemp2796@yahoo.com>
2019-10-29 19:26:50 -0400

    didd some stuff

--- client/src/components/Result.js
+++ client/src/components/Result.js
@@ -7,26 +7,28 @@ const Result = (props) => {
 		else return site.attributes.PERMITTYPE === props.permit
 	})
 	return (
-		filteredResults.map((site, index )=> (
-			<Link key={site.attributes.OBJECTID} to={`/sites/${site.attributes.OBJECTID}`}>
-				<div className="outer-wrapper">
-					<div className="inner-wrapper" key={index}>
-						<div className="image-wrapper" key={index}>
-							<img src={`https://maps.googleapis.com/maps/api/streetview?size=150x150&location=${site.attributes.HNUM}+${site.attributes.ST_NAME}+${site.attributes.PRE_DIR}+${site.attributes.ST_TYPE}+MIAMI+FL&heading=271&pitch=-0.76&key=AIzaSyDLun1DYQxp9IawieGnpd-4d0Jrp8sZSHU`} alt="contaminated site" />
-						</div>
-						<div className="text-wrapper">
-							<h3>Status: {site.attributes.CLASSIFCTN}</h3>
-							<h3>Type: {site.attributes.PERMITTYPE}</h3>
-							<h3>Phase: {site.attributes.PHASE}</h3>
-							<h3>Lat: {site.attributes.LAT}</h3>
-							<h3>Lon: {site.attributes.LON}</h3>
-							<h3>Cleanup: {site.attributes.TASK_NAME}</h3>
-							<h3>Address: {site.attributes.HNUM} {site.attributes.PRE_DIR} {site.attributes.ST_NAME} {site.attributes.ST_TYPE}</h3>
-						</div>
-					</div>
-				</div>
-			</Link>
-		))
+		<>
+			<h1 className="totalResults">Showing {filteredResults.length} of {props.sites.length} results for {props.permitText}</h1>
+			<div className="outer-wrapper">
+				{
+					filteredResults.map((site, index )=> (
+						<Link key={site.attributes.OBJECTID} to={`/sites/${site.attributes.OBJECTID}`}>
+							<div className="inner-wrapper" key={index}>
+								<div className="image-wrapper" key={index}>
+									<img className="google-image" src={`https://maps.googleapis.com/maps/api/streetview?size=400x400&location=${site.attributes.HNUM}+${site.attributes.ST_NAME}+${site.attributes.PRE_DIR}+${site.attributes.ST_TYPE}+MIAMI+FL&heading=271&pitch=-0.76&key=AIzaSyDLun1DYQxp9IawieGnpd-4d0Jrp8sZSHU`} alt="contaminated site" />
+								</div>
+								<div className="text-wrapper">
+									<h3>Status: {site.attributes.CLASSIFCTN}</h3>
+									<h3>Type: {site.attributes.PERMITTYPE}</h3>
+									<h3>Cleanup: {site.attributes.TASK_NAME}</h3>
+									<h3>Address: {site.attributes.HNUM} {site.attributes.PRE_DIR} {site.attributes.ST_NAME} {site.attributes.ST_TYPE}</h3>
+								</div>						
+							</div>
+						</Link>
+					))
+				}
+			</div>
+		</>
 	)
 } 
 
