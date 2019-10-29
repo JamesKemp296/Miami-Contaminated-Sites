c52d007c6b31621fd36dad81d74a539a43f28870
Alexia Panameno <alexiapanameno@gmail.com>
2019-10-29 19:10:15 -0400

    added bg and links about us

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,19 +4,9 @@ import React from 'react'
 const About = () => (
   <div>
 
-  <h1 className="about-title">About Us</h1>
+  <h1 className="about-title">ABOUT US</h1>
 
   <div className="about">
-    <a href="https://www.linkedin.com/in/victoriapanameno/">
-      <div>
-        <figure className="snip1104 red">
-          <img src='images/PanamenoAlexia.jpg' alt="sample33" height="350px"/>
-          <figcaption>
-            <h2>Alexia  <span> Panameno</span></h2>
-          </figcaption>
-        </figure>
-      </div>
-    </a>
     <a href="https://www.linkedin.com/in/annastepanova1811/">
       <div>
         <figure className="snip1104 red">
@@ -46,6 +36,16 @@ const About = () => (
         </figcaption>
       </figure>
     </div>
+    </a>
+    <a href="https://www.linkedin.com/in/victoriapanameno/">
+      <div>
+        <figure className="snip1104 red">
+          <img src='images/PanamenoAlexia.jpg' alt="sample33" height="350px"/>
+          <figcaption>
+            <h2>Alexia  <span> Panameno</span></h2>
+          </figcaption>
+        </figure>
+      </div>
     </a>
     <a href="https://www.linkedin.com/in/ashley-clayden-moss-b96b66181/">
       <div>
