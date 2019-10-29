616cc8b6c18e3de1b3bf76c443075b37d6608764
James Kemp <jameskemp2796@yahoo.com>
2019-10-29 17:11:12 -0400

    Moved the result component to a new file

--- client/src/pages/SearchResults.js
+++ client/src/pages/SearchResults.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import axios from 'axios'
+import Result from '../components/Result'
 
 class SearchResults extends React.Component {
   state = {
@@ -9,7 +9,8 @@ class SearchResults extends React.Component {
             sites: [],
             totalResults: 0,
             permit: 'all',
-            permitText: 'All'
+            permitText: 'All',
+            filteredResults: 0
           }
 
   handlePermitChange = event => this.setState({
@@ -39,12 +40,14 @@ class SearchResults extends React.Component {
     })
   }
 
+  
+
   render(){
     return(
       <>
         <div className="results-filters">
-
           <h1>{this.state.place.formatted_address}</h1>
+          <h1 className="totalResults">Showing {this.state.sites.length} of {this.state.totalResults} results for {this.state.permitText}</h1>
           <label htmlFor="radius-dropbox">Radius:</label>
           <select
             id="radius-dropbox"
@@ -71,34 +74,8 @@ class SearchResults extends React.Component {
             <option value="ARP">Airports and Contracts</option>
           </select>
         </div>
-          <h1 className="totalResults">Showing {this.state.sites.length} of {this.state.totalResults} results for {this.state.permitText}</h1>
-        {
-          this.state.sites
-          .filter(site => {
-            if (this.state.permit === 'all') return true
-            else return site.attributes.PERMITTYPE === this.state.permit
-          })
-          .map((site, index )=> (
-            <Link key={site.attributes.OBJECTID} to={`/sites/${site.attributes.OBJECTID}`}>
-              <div className="outer-wrapper">
-                <div className="inner-wrapper" key={index}>
-                  <div className="image-wrapper" key={index}>
-                    <img src={`https://maps.googleapis.com/maps/api/streetview?size=150x150&location=${site.attributes.HNUM}+${site.attributes.ST_NAME}+${site.attributes.PRE_DIR}+${site.attributes.ST_TYPE}+MIAMI+FL&heading=271&pitch=-0.76&key=AIzaSyDLun1DYQxp9IawieGnpd-4d0Jrp8sZSHU`} alt="contaminated site" />
-                  </div>
-                  <div className="text-wrapper">
-                    <h3>Status: {site.attributes.CLASSIFCTN}</h3>
-                    <h3>Type: {site.attributes.PERMITTYPE}</h3>
-                    <h3>Phase: {site.attributes.PHASE}</h3>
-                    <h3>Lat: {site.attributes.LAT}</h3>
-                    <h3>Lon: {site.attributes.LON}</h3>
-                    <h3>Cleanup: {site.attributes.TASK_NAME}</h3>
-                    <h3>Address: {site.attributes.HNUM} {site.attributes.PRE_DIR} {site.attributes.ST_NAME} {site.attributes.ST_TYPE}</h3>
-                  </div>
-                </div>
-              </div>
-            </Link>
-          ))
-        }
+        <h1 className="totalResults">Showing {this.state.sites.length} of {this.state.totalResults} results for {this.state.permitText}</h1>
+          <Result {...this.state}/>
         <div id="map"></div>
       </>
     )
