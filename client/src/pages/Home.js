f1cdcd1c231b7a92fff6701cf742c721f9931726
annastepanova <annastepanova1811@gmail.com>
2019-10-30 10:15:45 -0400

    made few corrections in css for homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,14 +5,14 @@ const Home = props => {
 	const goToSearchResultsForPlace = place => props.history.push(`/places/${place.place_id}`)
 
 	return(
-    <header class="v-header container">
-    <div class="fullscreen-video-wrap">
+    <header className="v-header container">
+    <div className="fullscreen-video-wrap">
       <video loop autoPlay>
       <source src="./video/Traffic.mp4" type="video/mp4" />
     </video>
     </div>
-    <div class="header-overlay">
-    <div class="header-content text-md-center">
+    <div className="header-overlay">
+    <div className="header-content">
       <h1 className="title">CONTAMINATED SITE SEARCH</h1>
       <h3 className="description">identifies properties where environmental contamination has been documented in the soil
       or groundwater. Facilities get listed as a contaminated site by a DERM inspector who finds a violation on the
@@ -21,8 +21,8 @@ const Home = props => {
       from the active contaminated sites layer/list when the sites is found by DERM to be cleaned up.
       Updated monthly.
       </h3>
-      <Autocomplete class="autocomplete"
-      style={{width: '40%'}}
+      <Autocomplete className="autocomplete"
+      style={{width: '35%'}}
       onPlaceSelected={goToSearchResultsForPlace}
       types={['address']}
       componentRestrictions={{ country: 'us' }}
@@ -34,4 +34,6 @@ const Home = props => {
 
 	)
 }
+
+
 export default Home
