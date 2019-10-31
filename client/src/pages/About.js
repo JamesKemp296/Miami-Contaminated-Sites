28fccb2d82d03f84e2c2bd7884e6d8e80e69eb66
James Kemp <jameskemp2796@yahoo.com>
2019-10-30 20:58:14 -0400

    fixed images

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react"
 
 
 const About = () => (
@@ -7,52 +7,52 @@ const About = () => (
     <div className="about">
       <div>
         <figure className="snip1104 red">
-          <img src='images/StepanovaAnna.jpg' alt="sample33" height="350px" />
+          <img src="images/StepanovaAnna.jpg" alt="sample33" height="350px" />
           <figcaption>
             <h2>Anna  <span> Stepanova</span></h2>
           </figcaption>
         </figure>
-        <a href="https://www.linkedin.com/in/annastepanova1811/" target="blank"><img src='images/LI-In-Bug.png' alt="linkedin" height="20px"/></a>
-        <a href="https://github.com/annastepanova" target="blank"><img src='images/Github-Mark-120px-plus.png' alt="github" height="20px" /></a>
+        <a href="https://www.linkedin.com/in/annastepanova1811/" target="blank"><img src="images/linkedIn.png" alt="linkedin" height="20px"/></a>
+        <a href="https://github.com/annastepanova" target="blank"><img src="images/github.png" alt="github" height="20px" /></a>
       </div>
       <div>
         <figure className="snip1104 red">
-          <img src='images/KempJames.jpg' alt="sample33" height="350px" />
+          <img src="images/KempJames.jpg" alt="sample33" height="350px" />
           <figcaption>
             <h2>James  <span> Kemp</span></h2>
           </figcaption>
         </figure>
-        <a href="https://www.linkedin.com/in/jamesdanielkemp/" target="blank"><img src='images/LI-In-Bug.png' alt="linkedin" height="20px"/></a>
-        <a href="https://github.com/JamesKemp296" target="blank"><img src='images/Github-Mark-120px-plus.png' alt="github" height="20px" /></a>
+        <a href="https://www.linkedin.com/in/jamesdanielkemp/" target="blank"><img src="images/linkedIn.png" alt="linkedin" height="20px"/></a>
+        <a href="https://github.com/JamesKemp296" target="blank"><img src="images/github.png" alt="github" height="20px" /></a>
       </div>
       <div>
         <figure className="snip1104 red">
-          <img src='images/ChunisAndrew.jpg' alt="sample33" height="350px" />
+          <img src="images/ChunisAndrew.jpg" alt="sample33" height="350px" />
           <figcaption>
             <h2>Andrew  <span> Chunis</span></h2>
           </figcaption>
         </figure>
-        <a href="https://www.linkedin.com/in/andrewchunis/" target="blank"><img src='images/LI-In-Bug.png' alt="linkedin" height="20px"/></a>
-        <a href="https://github.com/chuniversity" target="blank"><img src='images/Github-Mark-120px-plus.png' alt="github" height="20px" /></a>
+        <a href="https://www.linkedin.com/in/andrewchunis/" target="blank"><img src="images/linkedIn.png" alt="linkedin" height="20px"/></a>
+        <a href="https://github.com/chuniversity" target="blank"><img src="images/github.png" alt="github" height="20px" /></a>
       </div>
       <div>
         <figure className="snip1104 red">
-          <img src='images/PanamenoAlexia.jpg' alt="sample33" height="350px"/>
+          <img src="images/PanamenoAlexia.jpg" alt="sample33" height="350px"/>
           <figcaption>
             <h2>Alexia  <span> Panameno</span></h2>
           </figcaption>
         </figure>
-        <a href="https://www.linkedin.com/in/victoriapanameno/" target="blank"><img src='images/LI-In-Bug.png' alt="linkedin" height="20px" /></a>
-        <a href="https://github.com/rogueale" target="blank"><img src='images/Github-Mark-120px-plus.png' alt="github" height="20px" /></a>
+        <a href="https://www.linkedin.com/in/victoriapanameno/" target="blank"><img src="images/linkedIn.png" alt="linkedin" height="20px" /></a>
+        <a href="https://github.com/rogueale" target="blank"><img src="images/github.png" alt="github" height="20px" /></a>
       </div>
     <div>
         <figure className="snip1104 red">
-          <img src='images/ClaydenMossAshley.jpg' alt="sample33" height="350px" />
+          <img src="images/ClaydenMossAshley.jpg" alt="sample33" height="350px" />
           <figcaption>
             <h2>Ashley  <span> Clayden Moss</span></h2>
           </figcaption>
         </figure>
-        <a href="https://www.linkedin.com/in/ashley-clayden-moss-b96b66181/" target="blank"><img src='images/LI-In-Bug.png' alt="linkedin" height="20px"></img></a>
+        <a href="https://www.linkedin.com/in/ashley-clayden-moss-b96b66181/" target="blank"><img src="images/linkedIn.png" alt="linkedin" height="20px"></img></a>
         <a href="http://daffodil-cat-6xa5.squarespace.com/" target="blank"><img src="images/Squarespace.png" alt="squarespace" height="20px" /></a>
       </div>
     </div>
